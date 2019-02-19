b62cba36a9c2cdf575a198ba9761760cda2d21c6
eboury <elie.boury@etu.u-bordeaux.fr>
2019-02-19 15:00:18 +0100

    Création des dossierAnimal controller et fxml

--- Veto/src/main/java/pt4p1ae1/veto/GestionAnimaux/DossierAnimalController.java
+++ Veto/src/main/java/pt4p1ae1/veto/GestionAnimaux/DossierAnimalController.java
@@ -6,7 +6,7 @@ import pt4p1ae1.veto.ControllerSample;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class AnimauxController extends ControllerSample implements Initializable {
+public class DossierAnimalController extends ControllerSample implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         super.start();
