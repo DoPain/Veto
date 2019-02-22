bf4fe626082a424fe51d6aa1097492a1ccfb3af4
Aragon Nathan <nathan.aragon@etu.u-bordeaux.fr>
2019-02-22 09:42:58 +0100

    ClientController comme avant

--- Veto/src/main/java/pt4p1ae1/veto/GestionCLient/ClientController.java
+++ Veto/src/main/java/pt4p1ae1/veto/GestionCLient/ClientController.java
@@ -5,10 +5,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import pt4p1ae1.veto.ControllerSample;
-import pt4p1ae1.veto.Entity.Client;
+import pt4p1ae1.veto.DataBase;
 
 import java.net.URL;
-import java.util.List;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 
 public class ClientController extends ControllerSample implements Initializable {
@@ -18,11 +18,27 @@ public class ClientController extends ControllerSample implements Initializable 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        //TODO : récupérer les clients comme en JEE
-
-        for (Client c : clients) {
-//            ClientBox.getChildren().add(new Button(c.getPersonne().getIdPersonne() + ". " + c.getPersonne().getNom() + ", " + c.getPersonne().getPrenom()));
-            ClientBox.getChildren().add(new Button( String.valueOf(c.getIdClient())));
+        try {
+            DataBase dataBase = new DataBase();
+            ResultSet rs = dataBase.getClient();
+            while (rs.next()) {
+                String s = rs.getString("NOM")
+                        + " "
+                        + rs.getString("PRENOM")
+                        + " "
+                        + rs.getString("ADRESSE")
+                        + " "
+                        + rs.getString("MAIL")
+                        + " "
+                        + rs.getString("TEL")
+                        + " "
+                        + rs.getString("VILLE");
+                Button a = new Button(s);
+                a.setOnAction(e -> afficherClient(s));
+                ClientBox.getChildren().add(a);
+            }
+        } catch (Exception e){
+            System.out.println("Inizialisation LogController :" + e);
         }
         super.start();
     }
