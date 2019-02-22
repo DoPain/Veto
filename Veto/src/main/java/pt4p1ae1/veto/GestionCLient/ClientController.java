7d180d214966bc4cea16d87011a2a5f8138004f0
Aragon Nathan <nathan.aragon@etu.u-bordeaux.fr>
2019-02-22 08:53:24 +0100

    generated Entities

--- Veto/src/main/java/pt4p1ae1/veto/GestionCLient/ClientController.java
+++ Veto/src/main/java/pt4p1ae1/veto/GestionCLient/ClientController.java
@@ -4,11 +4,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import pt4p1ae1.veto.ControllerSample;
 import pt4p1ae1.veto.Entity.Client;
-import pt4p1ae1.veto.HibernateUtil;
 
 import java.net.URL;
 import java.util.List;
@@ -21,13 +18,7 @@ public class ClientController extends ControllerSample implements Initializable 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        Session session = sessionFactory.openSession();
-
-        List<Client> clients = session.createQuery("from Client").list();
-
-        session.close();
+        //TODO : récupérer les clients comme en JEE
 
         for (Client c : clients) {
 //            ClientBox.getChildren().add(new Button(c.getPersonne().getIdPersonne() + ". " + c.getPersonne().getNom() + ", " + c.getPersonne().getPrenom()));
