4521838b0418f556fc770c9f34b0bc93aa0638de
bastien <bastien.vincent41@yahoo.com>
2019-03-25 17:25:09 +0100

    maj Home, start new Home

--- Veto/src/main/java/pt4p1ae1/veto/HomePage/HomeController.java
+++ Veto/src/main/java/pt4p1ae1/veto/HomePage/HomeController.java
@@ -1,14 +1,23 @@
 package pt4p1ae1.veto.HomePage;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+import jfxtras.internal.scene.control.skin.agenda.AgendaDaySkin;
+import jfxtras.internal.scene.control.skin.agenda.AgendaSkin;
+import jfxtras.scene.control.agenda.Agenda;
 import pt4p1ae1.veto.ControllerSample;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HomeController extends ControllerSample implements Initializable {
+
+    @FXML BorderPane borderPane;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         super.start();
+
     }
 }
