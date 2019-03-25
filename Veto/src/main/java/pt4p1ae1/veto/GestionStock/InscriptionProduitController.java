4c67b19f8ff249bfc8c8116bf274727d063e0eca
Potin Mateo <mateo.potin@etu.u-bordeaux.fr>
2019-03-25 17:21:41 +0100

    bug agenda

--- Veto/src/main/java/pt4p1ae1/veto/GestionStock/InscriptionProduitController.java
+++ Veto/src/main/java/pt4p1ae1/veto/GestionStock/InscriptionProduitController.java
@@ -14,6 +14,9 @@ import java.net.URL;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -34,9 +37,9 @@ public class InscriptionProduitController extends ControllerSample implements In
     @FXML
     private TextField nbInsertion;
     @FXML
-    private TextField dateAcquisition;
+    private DatePicker dateAcquisition;
     @FXML
-    private TextField datePeremption;
+    private DatePicker datePeremption;
     @FXML
     private TextField minQuantite;
     @FXML
@@ -58,7 +61,7 @@ public class InscriptionProduitController extends ControllerSample implements In
     private void ajouterProduit(ActionEvent actionEvent){
         Boolean found = false;
         int i = 0;
-        if(!nom.getText().equals("") && !minQuantite.getText().equals("") && !reference.getText().equals("") && !prix.getText().equals("") && !nbInsertion.getText().equals("") && !dateAcquisition.getText().equals("") && !datePeremption.getText().equals("")){
+        if(!nom.getText().equals("") && !minQuantite.getText().equals("") && !reference.getText().equals("") && !prix.getText().equals("") && !nbInsertion.getText().equals("") && dateAcquisition.getValue() != null && datePeremption.getValue() != null){
             List<ProduitEntity> produits = Utils.PRODUIT_DAO.findAll();
             while(!found && i<produits.size()){
                 if(produits.get(i).getNom().equals(nom.getText())){
@@ -79,17 +82,13 @@ public class InscriptionProduitController extends ControllerSample implements In
                 newProduit.setRefProduit(reference.getText());
                 DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy",Locale.FRANCE);
 
-                try {
-                    Date dateP  = formatter.parse(datePeremption.getText());
-                    Date dateA = formatter.parse(dateAcquisition.getText());
-                    java.sql.Date sqlDateA = new java.sql.Date(dateA.getTime());
-                    java.sql.Date sqlDateP = new java.sql.Date(dateP.getTime());
+                LocalDate localDateAcqui = dateAcquisition.getValue();
+                LocalDate localDatePer = datePeremption.getValue();
+                    java.sql.Date sqlDateA = java.sql.Date.valueOf(localDateAcqui);
+                    java.sql.Date sqlDateP = java.sql.Date.valueOf(localDatePer);
                     newProduit.setDateAcquisition(sqlDateA);
                     newProduit.setPeremption(sqlDateP);
-                } catch (ParseException e) {
-                    error.setText("Date(s) invalide(s)");
-                    error.setText("Information(s) manquante(s)");
-                }
+
                 Utils.PRODUIT_DAO.saveOrUpdate(newProduit);
             }
         } else {
