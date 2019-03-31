5a82c03b0fe757b6d7c0e2be9c105abb9c82b471
bastien <bastien.vincent41@yahoo.com>
2019-03-31 20:45:16 +0200

    Ajout de la javadoc

--- Veto/src/main/java/pt4p1ae1/veto/AgendaPage/AgendaPage.java
+++ Veto/src/main/java/pt4p1ae1/veto/AgendaPage/AgendaPage.java
@@ -27,6 +27,9 @@ import java.time.LocalDateTime;
 import java.time.Period;
 import java.util.*;
 
+/**
+ * controlleur de la page agenda, qui affiche donc l'agenda et les rendez-vous
+ */
 public class AgendaPage extends ControllerSample implements Initializable {
 
     @FXML
@@ -60,6 +63,15 @@ public class AgendaPage extends ControllerSample implements Initializable {
     private AnimalEntity selectedAnimal;
     public static HashMap<String, RendezVousEntity> vEventEntity = new HashMap<>();
 
+
+    /**
+     * créer le calendrier et l'agenda
+     * initialise le date picker à la date du jour
+     * et appelle les différents fonctions d'initialisation de la page
+     *
+     * @param location
+     * @param resources
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         vCalendar = new VCalendar();
@@ -70,9 +82,13 @@ public class AgendaPage extends ControllerSample implements Initializable {
         super.start();
         setActionButton();
         initializeAgenda();
+        initializeChoiceBox();
         addAllEvent();
     }
 
+    /**
+     * ajoute tous les rendez-vous de la base de donnée dans le calendrier
+     */
     private void addAllEvent() {
         List<VEvent> vEventList = new ArrayList<>();
         Utils.RENDEZ_VOUS_DAO.findAll().forEach(entity -> {
@@ -86,6 +102,10 @@ public class AgendaPage extends ControllerSample implements Initializable {
         agendaHome.getVCalendar().setVEvents(vEventList);
     }
 
+    /**
+     * modify l'action de selection d'un rendez-vous
+     * -> affiche le menu de modification du rendez-vous
+     */
     private void initializeAgenda() {
         agendaHome.setSelectedOneAppointmentCallback(param ->
                 {
@@ -98,16 +118,14 @@ public class AgendaPage extends ControllerSample implements Initializable {
                     if(vEventEntity.containsKey(selectedVEvent.getUniqueIdentifier().getValue())){
                         selectedRDVentity = vEventEntity.get(selectedVEvent.getUniqueIdentifier().getValue());
                         vetoBox.setValue(selectedRDVentity.getVeterinaireByIdVeterinaire());
-                        System.out.println(selectedRDVentity.getIdAnimal());
-//                        Utils.ANIMAL_DAO.findAll().forEach(animalEntity -> {
-//                                if(animalEntity.getId()==selectedRDVentity.getIdAnimal()){
-//                                    selectedAnimal = animalEntity;
-//                                }
-//                        });
-//                        AnimalEntity animal = Utils.ANIMAL_DAO.findById(selectedRDVentity.getIdAnimal());
-//                        ClientEntity client = animal.getClientByIdClient();
-//                        clientBox.setValue(client);
-//                        animalBox.setValue(animal);
+                        Utils.ANIMAL_DAO.findAll().forEach(animalEntity -> {
+                                if(animalEntity.getId()==selectedRDVentity.getIdAnimal()){
+                                    selectedAnimal = animalEntity;
+                                }
+                        });
+                        ClientEntity client = selectedAnimal.getClientByIdClient();
+                        clientBox.setValue(client);
+                        animalBox.setValue(selectedAnimal);
                     }else{
                         selectedRDVentity = RendezVousEntityOservable.toEntity(selectedVEvent);
                         vetoBox.setValue(null);
@@ -115,27 +133,15 @@ public class AgendaPage extends ControllerSample implements Initializable {
                         animalBox.setValue(null);
                     }
                     modifyEvent.setDisable(false);
-
-                    modifyButton.setOnAction(event -> {
-                        if (animalBox.getValue() != null && clientBox.getValue() != null) {
-                            selectedRDVentity.setIdAnimal(animalBox.getValue().getId()); //recup l'id de l'animal
-                            selectedRDVentity.setIdVeterinaire(vetoBox.getValue().getId()); //recup l'id du client
-                            selectedRDVentity.setDescription("Rendez vous :\n" +clientBox.getValue()+"\n" +animalBox.getValue());
-                            selectedVEvent.setDescription("Rendez vous :\n" +clientBox.getValue()+"\n" +animalBox.getValue());
-                            vEventEntity.put(selectedVEvent.getUniqueIdentifier().getValue(), selectedRDVentity);
-                            saveEvents();
-                            agendaHome.refresh();
-                            messageAfterModif.setTextFill(Color.web("#15e246"));
-                            messageAfterModif.setText("Rendez Vous Ajouté");
-                        }else{
-                            messageAfterModif.setTextFill(Color.web("#e23a15"));
-                            messageAfterModif.setText("Champs invalides");
-                        }
-                    });
                     return null;
-                }
-        );
+                });
+    }
 
+    /**
+     * Rempli les choices boxs veto et client,
+     * et ajoute l'actions de remplissage de la choice box animal lors du choix d'un client
+     */
+    private void initializeChoiceBox(){
         ArrayList<VeterinaireEntity> listVeto = new ArrayList<>(Utils.VETERINAIRE_DAO.findAll());
         ObservableList<VeterinaireEntity> vetoEntities = FXCollections.observableArrayList(listVeto);
         vetoBox.setItems(vetoEntities);
@@ -145,13 +151,18 @@ public class AgendaPage extends ControllerSample implements Initializable {
         clientBox.setItems(clientEntities);
 
         clientBox.setOnAction(event -> {
-            ClientEntity client = (ClientEntity) clientBox.getValue();
-            ArrayList<AnimalEntity> listAnimal = new ArrayList<>(Utils.getAnimalFromClient(client.getId()));
-            ObservableList<AnimalEntity> animalEntities = FXCollections.observableArrayList(listAnimal);
-            animalBox.setItems(animalEntities);
+            if(clientBox.getValue()!=null){
+                ClientEntity client = (ClientEntity) clientBox.getValue();
+                ArrayList<AnimalEntity> listAnimal = new ArrayList<>(Utils.getAnimalFromClient(client.getId()));
+                ObservableList<AnimalEntity> animalEntities = FXCollections.observableArrayList(listAnimal);
+                animalBox.setItems(animalEntities);
+            }
         });
     }
 
+    /**
+     * Ajoute les fonctions d'actions à tous les boutons de la page agenda
+     */
     private void setActionButton() {
         increase_btn.setOnAction(event -> {
             LocalDateTime newLocalDateTime = agendaHome.getDisplayedLocalDateTime().plus(Period.ofWeeks(1));
@@ -162,6 +173,7 @@ public class AgendaPage extends ControllerSample implements Initializable {
         decrease_btn.setOnAction(event -> {
             LocalDateTime newLocalDateTime = agendaHome.getDisplayedLocalDateTime().minus(Period.ofWeeks(1));
             agendaHome.setDisplayedLocalDateTime(newLocalDateTime);
+
             datePicker.setValue(newLocalDateTime.toLocalDate());
         });
 
@@ -174,8 +186,28 @@ public class AgendaPage extends ControllerSample implements Initializable {
             agendaHome.setDisplayedLocalDateTime(newLocalDateTime);
         });
 
+        modifyButton.setOnAction(event -> {
+            if (vetoBox.getValue() != null && animalBox.getValue() != null && clientBox.getValue() != null) {
+                selectedRDVentity.setIdAnimal(animalBox.getValue().getId()); //recup l'id de l'animal
+                selectedRDVentity.setIdVeterinaire(vetoBox.getValue().getId()); //recup l'id du client
+                selectedRDVentity.setDescription("Rendez vous :\n" +clientBox.getValue()+"\n" +animalBox.getValue());
+                selectedVEvent.setDescription("Rendez vous :\n" +clientBox.getValue()+"\n" +animalBox.getValue());
+                vEventEntity.put(selectedVEvent.getUniqueIdentifier().getValue(), selectedRDVentity);
+                saveEvents();
+                agendaHome.refresh();
+                messageAfterModif.setTextFill(Color.web("#15e246"));
+                messageAfterModif.setText("Rendez Vous Ajouté");
+            }else{
+                messageAfterModif.setTextFill(Color.web("#e23a15"));
+                messageAfterModif.setText("Champs invalides");
+            }
+        });
+
     }
 
+    /**
+     * sauvegarde les rendez-vous dans la base de données
+     */
     static public void saveEvents() {
         List<VEvent> vEvents = agendaHome.getVCalendar().getVEvents();
         Utils.RENDEZ_VOUS_DAO.removeAll();
@@ -191,21 +223,30 @@ public class AgendaPage extends ControllerSample implements Initializable {
                     }
                     Utils.RENDEZ_VOUS_DAO.saveOrUpdate(rdv);
                 });
-                List<String> eventRemove = new ArrayList<>();
-                vEventEntity.keySet().forEach(s -> {
-                    boolean exist = false;
-                    Iterator<VEvent> iterator = vEvents.iterator();
-                    while (!exist && iterator.hasNext()) {
-                        exist = iterator.next().getUniqueIdentifier().getValue().equals(s);
-                    }
-                    if (!exist) {
-                        eventRemove.add(s);
-                    }
-                });
-                eventRemove.forEach(eventUI -> {
-                    vEventEntity.remove(eventUI);
-                });
+                removeSecialEvent(vEvents);
             }
         }
+    }
+
+    /**
+     * enlève les rendez-vous avec client qui ont était supprimer de l'agenda
+     * et qui sont stockés dans la map vEventEntity
+     * @param vEvents la liste des rendez-vous à supprimer
+     */
+    static private void removeSecialEvent(List<VEvent> vEvents){
+        List<String> eventRemove = new ArrayList<>();
+        vEventEntity.keySet().forEach(s -> {
+            boolean exist = false;
+            Iterator<VEvent> iterator = vEvents.iterator();
+            while (!exist && iterator.hasNext()) {
+                exist = iterator.next().getUniqueIdentifier().getValue().equals(s);
+            }
+            if (!exist) {
+                eventRemove.add(s);
+            }
+        });
+        eventRemove.forEach(eventUI -> {
+            vEventEntity.remove(eventUI);
+        });
     }
 }
